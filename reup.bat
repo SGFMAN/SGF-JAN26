923bfbd7f4438a13bc7031f6c5c69abdf0703650
Ben Donnan <ben@superiorgrannyflats.com.au>
2026-05-28 14:02:22 +1000

    ben11

---
+++ reup.bat
@@ -0,0 +1,20 @@
+@echo off
+set MSG=%*
+
+if "%MSG%"=="" (
+  echo Please enter a commit message.
+  echo Example: REUP version1
+  exit /b 1
+)
+
+cd /d C:\SGF
+
+git add .
+git commit -m "%MSG%"
+
+cd /d C:\SGF\frontend
+npm run build
+
+cd /d C:\SGF\backend
+pm2 restart sgfcentral
+pm2 status
